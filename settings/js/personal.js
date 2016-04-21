786dde9389be4ed9d790a905234ae9708f03f65d
Christoph Wurst <christoph@owncloud.com>
2016-04-21 10:33:03 +0200

    Add federation scope to the user avatar

--- settings/js/personal.js
+++ settings/js/personal.js
@@ -137,7 +137,7 @@ function avatarResponseHandler (data) {
 	if (typeof data === 'string') {
 		data = JSON.parse(data);
 	}
-	var $warning = $('#avatar .warning');
+	var $warning = $('#avatarform .warning');
 	$warning.hide();
 	if (data.status === "success") {
 		updateAvatar();
@@ -217,7 +217,7 @@ $(document).ready(function () {
 	});
 
 	var federationSettingsView = new OC.Settings.FederationSettingsView({
-		el: '#personal-settings-container'
+		el: '#personal-settings'
 	});
 	federationSettingsView.render();
 
@@ -355,7 +355,7 @@ $(document).ready(function () {
 
 	// Load the big avatar
 	if (oc_config.enable_avatars) {
-		$('#avatar .avatardiv').avatar(OC.currentUser, 145);
+		$('#avatarform .avatardiv').avatar(OC.currentUser, 145);
 	}
 
 	// Show token views
