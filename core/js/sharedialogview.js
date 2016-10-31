theSoenke/nextcloud-server

introduce share by mail, ui part

core/js/sharedialogview.js
@@ -152,10 +152,12 @@
 						var users   = result.ocs.data.exact.users.concat(result.ocs.data.users);
 						var groups  = result.ocs.data.exact.groups.concat(result.ocs.data.groups);
 						var remotes = result.ocs.data.exact.remotes.concat(result.ocs.data.remotes);
+						var emails = result.ocs.data.exact.emails.concat(result.ocs.data.emails);
 
 						var usersLength;
 						var groupsLength;
 						var remotesLength;
+						var emailsLength;
 
 						var i, j;
 
@@ -210,10 +212,18 @@
 										break;
 									}
 								}
+							} else if (share.share_type === OC.Share.SHARE_TYPE_EMAIL) {
+							emailsLength = emails.length;
+							for (j = 0; j < emailsLength; j++) {
+								if (emails[j].value.shareWith === share.share_with) {
+									emails.splice(j, 1);
+									break;
+								}
 							}
 						}
+						}
 
-						var suggestions = users.concat(groups).concat(remotes);
+						var suggestions = users.concat(groups).concat(remotes).concat(emails);
 
 						if (suggestions.length > 0) {
 							$('.shareWithField').removeClass('error')
@@ -266,6 +276,10 @@
 						sharee: text
 					});
 				}
+			} else if (item.value.shareType === OC.Share.SHARE_TYPE_EMAIL) {
+				text = t('core', '{sharee} (email)', {
+					sharee: text
+				});
 			}
 			insert.text(text);
 			insert.attr('title', item.value.shareWith);
@@ -390,7 +404,7 @@
 				var infoTemplate = this._getRemoteShareInfoTemplate();
 				remoteShareInfo = infoTemplate({
 					docLink: this.configModel.getFederatedShareDocLink(),
-					tooltip: t('core', 'Share with people on other ownClouds using the syntax username@example.com/owncloud')
+					tooltip: t('core', 'Share with people on other servers using the syntax username@example.com/owncloud')
 				});
 			}
 
@@ -398,16 +412,16 @@
 		},
 
 		_renderSharePlaceholderPart: function () {
-			var sharePlaceholder = t('core', 'Share with users…');
+			var sharePlaceholder = t('core', 'Share with users, or by mail...');
 
 			if (this.configModel.get('allowGroupSharing')) {
 				if (this.configModel.get('isRemoteShareAllowed')) {
-					sharePlaceholder = t('core', 'Share with users, groups or remote users…');
+					sharePlaceholder = t('core', 'Share with users, groups, remote users, or by mail…');
 				} else {
-					sharePlaceholder = t('core', 'Share with users or groups…')
+					sharePlaceholder = t('core', 'Share with users, groups or by mail...')
 				}
 			} else if (this.configModel.get('isRemoteShareAllowed')) {
-					sharePlaceholder = t('core', 'Share with users or remote users…');
+					sharePlaceholder = t('core', 'Share with users, remote users or by mail...');
 			}
 
 			return sharePlaceholder;

pinos/server

Signed-off-by: Bjoern Schiessle <bjoern@schiessle.org>

core/js/sharedialogview.js
@@ -154,10 +154,12 @@
 						var users   = result.ocs.data.exact.users.concat(result.ocs.data.users);
 						var groups  = result.ocs.data.exact.groups.concat(result.ocs.data.groups);
 						var remotes = result.ocs.data.exact.remotes.concat(result.ocs.data.remotes);
+						var emails = result.ocs.data.exact.emails.concat(result.ocs.data.emails);
 
 						var usersLength;
 						var groupsLength;
 						var remotesLength;
+						var emailsLength;
 
 						var i, j;
 
@@ -212,10 +214,18 @@
 										break;
 									}
 								}
+							} else if (share.share_type === OC.Share.SHARE_TYPE_EMAIL) {
+							emailsLength = emails.length;
+							for (j = 0; j < emailsLength; j++) {
+								if (emails[j].value.shareWith === share.share_with) {
+									emails.splice(j, 1);
+									break;
+								}
 							}
 						}
+						}
 
-						var suggestions = users.concat(groups).concat(remotes);
+						var suggestions = users.concat(groups).concat(remotes).concat(emails);
 
 						if (suggestions.length > 0) {
 							$('.shareWithField').removeClass('error')
@@ -268,6 +278,10 @@
 						sharee: text
 					});
 				}
+			} else if (item.value.shareType === OC.Share.SHARE_TYPE_EMAIL) {
+				text = t('core', '{sharee} (email)', {
+					sharee: text
+				});
 			}
 			var insert = $("<div class='share-autocomplete-item'/>");
 			var avatar = $("<div class='avatardiv'></div>").appendTo(insert);
@@ -397,7 +411,7 @@
 				var infoTemplate = this._getRemoteShareInfoTemplate();
 				remoteShareInfo = infoTemplate({
 					docLink: this.configModel.getFederatedShareDocLink(),
-					tooltip: t('core', 'Share with people on other Nextclouds using the syntax username@example.com/nextcloud')
+					tooltip: t('core', 'Share with people on other servers using the syntax username@example.com/nextcloud')
 				});
 			}
 
@@ -405,16 +419,16 @@
 		},
 
 		_renderSharePlaceholderPart: function () {
-			var sharePlaceholder = t('core', 'Share with users…');
+			var sharePlaceholder = t('core', 'Share with users, or by mail...');
 
 			if (this.configModel.get('allowGroupSharing')) {
 				if (this.configModel.get('isRemoteShareAllowed')) {
-					sharePlaceholder = t('core', 'Share with users, groups or remote users…');
+					sharePlaceholder = t('core', 'Share with users, groups, remote users, or by mail…');
 				} else {
-					sharePlaceholder = t('core', 'Share with users or groups…');
+					sharePlaceholder = t('core', 'Share with users, groups or by mail...');
 				}
 			} else if (this.configModel.get('isRemoteShareAllowed')) {
-					sharePlaceholder = t('core', 'Share with users or remote users…');
+					sharePlaceholder = t('core', 'Share with users, remote users or by mail...');
 			}
 
 			return sharePlaceholder;